00556925a66532b57e7af1c4408c45365425f5e8
Dedu Madalina <madalina.dedu@yahoo.com>
2023-01-24 00:16:13 +0200

    Am adaugat map

---
+++ map.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home</title>
+
+    <link rel="stylesheet" type="text/css" href="map2.css" />
+    <script src="map1.js"></script>
+  </head>
+
+  <body>
+
+    <div id="map"></div>
+    <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdhIibhgD14684u3hdnJtLwBW0sZ8M14k&callback=initMap">
+  </script>
+
+  </body>
+</html>
