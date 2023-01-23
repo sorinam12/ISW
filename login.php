defc1924e3a96d2f7d796209d2954066b9c6b245
Dedu Madalina <madalina.dedu@yahoo.com>
2023-01-24 00:01:44 +0200

    Am adaugat login

---
+++ login.php
@@ -0,0 +1,55 @@
+
+<html>
+   
+<head>
+	<title> login and registeration form design </title>
+    <link rel= "stylesheet" type="text/css" href="styleLogin.css">
+	<link rel= "stylesheet" type="text/css"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+
+   <body>
+<div class="container">
+<div class="login-box">
+<div class="square" style="--i:0";></div>
+   <div class="square" style="--i:1";></div>
+   <div class="square" style="--i:2";></div>
+   <div class="square" style="--i:3";></div>
+   <div class="square" style="--i:4";></div>
+   <div class="square" style="--i:5";></div>
+   <div class="square" style="--i:6";></div>
+<div class="register-form">
+
+		<div class="row">
+		<div class="col-md-6 login-left">
+      
+      <h2>Login Here</h2> 
+         
+      
+      
+      
+      
+      
+         <form  method = "post">
+	<div class= "form-group">
+		<label> Username </label>
+            <input type = "text" name = "username" class= "form-control">
+	</div>
+	<div class= "form-group">
+		<label> Password </label>
+            <input type = "password" name = "password" class= "form-control" required>
+	</div>
+            <button type = "submit" name = "login" class="btn">Login</button>
+         </form>
+	</div> 
+
+
+
+
+
+
+		</div> 
+      </div> 
+      </div>
+   </body>
+</html>
