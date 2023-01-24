faef430e5dd2d2408e179407c00dc88660e2dbeb
Sorina Manole <sorinamanole12@gmail.com>
2023-01-24 11:45:13 +0200

    Open when someone clicks on the span element

--- script_Cinema.js
+++ script_Cinema.js
@@ -286,4 +286,65 @@ function openNav(movie) {
     });
 }
 
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
+var activeSlide = 0;
+var totalVideos = 0;
+
+function showVideos() {
+  let embedClasses = document.querySelectorAll(".embed");
+  let dots = document.querySelectorAll(".dot");
+
+  totalVideos = embedClasses.length;
+  embedClasses.forEach((embedTag, idx) => {
+    if (activeSlide == idx) {
+      embedTag.classList.add("show");
+      embedTag.classList.remove("hide");
+    } else {
+      embedTag.classList.add("hide");
+      embedTag.classList.remove("show");
+    }
+  });
+
+  dots.forEach((dot, indx) => {
+    if (activeSlide == indx) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
+    }
+  });
+}
+
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+leftArrow.addEventListener("click", () => {
+  if (activeSlide > 0) {
+    activeSlide--;
+  } else {
+    activeSlide = totalVideos - 1;
+  }
+
+  showVideos();
+});
+
+rightArrow.addEventListener("click", () => {
+  if (activeSlide < totalVideos - 1) {
+    activeSlide++;
+  } else {
+    activeSlide = 0;
+  }
+  showVideos();
+});
+
+function getColor(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
